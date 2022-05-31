b789253cfc3c48f86aec054b06d7f22627cea857
Victor Moraes <victorgabrielvgfm@gmail.com>
2022-05-30 22:44:06 -0300

    Add MainPage

---
+++ src/pages/MainPage/MainPage.tsx
@@ -0,0 +1,14 @@
+import { MainPageDiv } from './styles'
+import Header from '../../components/Header'
+import SocialMediaBar from '../../components/SocialMediaBar'
+
+const MainPage = () => {
+  return (
+    <MainPageDiv>
+      <SocialMediaBar />
+      <Header />
+    </MainPageDiv>
+  )
+}
+
+export default MainPage
---
+++ src/pages/MainPage/styles.ts
@@ -0,0 +1,9 @@
+import styled from 'styled-components'
+
+export const MainPageDiv = styled.div`
+  display: grid;
+  grid-template-columns: 117px 1fr;
+  grid-template-rows: 50% 50%;
+
+  height: 100%;
+`
